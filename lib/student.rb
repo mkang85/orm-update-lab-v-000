4881151afb15255784d99f3d7d24780f1391b6a6
Michael Kang <michaelkang85@gmail.com>
2018-04-06 00:44:09 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -58,7 +58,9 @@ attr_reader :id
     FROM students
     WHERE students.name = name
     SQL
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
 
 end
